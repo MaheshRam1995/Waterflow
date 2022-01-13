dbfd8e97e26ee8e098de58a566d835e1d994c853
MaheshRam1995 <maheshchandrancse@gmail.com>
2022-01-13 11:53:42 +0530

    Index commit

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './css/index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
